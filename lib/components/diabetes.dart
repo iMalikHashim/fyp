31afd211926d5882b3e2c39250d777373d2879e3
OyeeHashi <hashimtop10@gmail.com>
2023-12-07 22:52:49 +0500

    functions

--- lib/components/diabetes.dart
+++ lib/components/diabetes.dart
@@ -8,4 +8,25 @@ class DiabetesScreen extends StatefulWidget {
 class _DiabetesScreenState extends State<DiabetesScreen> {
   double bloodSugarLevel = 100.0;
   double gfrRate = 90.0;
+
+  void updateBloodSugarLevel(double newLevel) {
+    setState(() {
+      bloodSugarLevel = newLevel;
+    });
+  }
+
+  void updateGFRRate(double newRate) {
+    setState(() {
+      gfrRate = newRate;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Diabetes and Kidney Monitoring'),
+      ),
+    );
+  }
 }
