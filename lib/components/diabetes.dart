17d1a2c123164994d265b09f776818516cf697d6
OyeeHashi <hashimtop10@gmail.com>
2023-12-07 22:51:40 +0500

    1st draft

---
+++ lib/components/diabetes.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+class DiabetesScreen extends StatefulWidget {
+  @override
+  _DiabetesScreenState createState() => _DiabetesScreenState();
+}
--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:fl_chart/fl_chart.dart';
 import 'components/dashboard.dart';
 import 'components/Reports.dart';
+import 'components/diabetes.dart';
 
 void main() => runApp(MyApp());
 
