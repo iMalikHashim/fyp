e6a06f5013f2ba308bc3df5f9ef7a6ffe9e6af4d
Hania Maryam Riaz <hriaz.bscs20seecs@seecs.edu.pk>
2023-12-11 06:40:04 +0500

    addReports inprogress

--- lib/components/addReports.dart
+++ lib/components/addReports.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: prefer_const_constructors
+
 import 'package:flutter/material.dart';
 
 class AddReportScreen extends StatefulWidget {
@@ -8,8 +10,164 @@ class AddReportScreen extends StatefulWidget {
 }
 
 class _AddReportScreenState extends State<AddReportScreen> {
+  int selectedButtonIndex = 0;
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        title: Text(
+          "NephroFit",
+          style: TextStyle(
+              color: Colors.black, // Set text color to black
+              fontWeight: FontWeight.bold,
+              fontSize: 30.0),
+        ),
+        centerTitle: true,
+        backgroundColor: Colors.white, // Set background color to white
+        elevation: 0.0,
+        actions: [
+          IconButton(
+            icon: Icon(
+              Icons.notifications,
+              color: Colors.black,
+            ),
+            onPressed: () {
+              // Add your notification icon onPressed logic here
+            },
+          ),
+        ],
+      ),
+      body: Column(
+        // mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Center(
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                SizedBox(
+                  height: 90.0,
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    setState(() {
+                      selectedButtonIndex = 0;
+                    });
+                    // Navigate to the ViewReports screen using the named route
+                    Navigator.pushNamed(context, '/addReports');
+                  },
+                  style: ElevatedButton.styleFrom(
+                    primary: selectedButtonIndex == 0
+                        ? Colors.white
+                        : Color(0xFF055B5C),
+                    onPrimary: Colors.black,
+                  ),
+                  child: Text('Add Reports'),
+                ),
+                SizedBox(
+                  width: 4.0,
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    setState(() {
+                      selectedButtonIndex = 1;
+                    });
+                    // Navigate to the ViewReports screen using the named route
+                    Navigator.pushNamed(context, '/viewReports');
+                  },
+                  style: ElevatedButton.styleFrom(
+                    primary: selectedButtonIndex == 1
+                        ? Colors.white
+                        : Color(0xFF055B5C),
+                    onPrimary: Colors.black,
+                  ),
+                  child: Text('View Reports'),
+                ),
+              ],
+            ),
+          ),
+          Center(
+            child: Container(
+              width: 300,
+              child: Column(
+                children: [
+                  TextField(
+                    decoration: InputDecoration(
+                      labelText: 'Enter Blood Pressure',
+                      hintText: 'For example 120/80',
+                      border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(11),
+                          borderSide: BorderSide(
+                            color: Color(0xFF055B5C),
+                          )),
+                      focusedBorder: OutlineInputBorder(
+                        // borderRadius: BorderRadius.circular(11.),
+                        borderSide: BorderSide(color: Color(0xFF055B5C)),
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  TextField(
+                    decoration: InputDecoration(
+                      labelText: 'Enter Sugar Level ',
+                      hintText: 'For example 70mg/dL',
+                      border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(11),
+                          borderSide: BorderSide(
+                            color: Color(0xFF055B5C),
+                          )),
+                      focusedBorder: OutlineInputBorder(
+                        // borderRadius: BorderRadius.circular(11.),
+                        borderSide: BorderSide(color: Color(0xFF055B5C)),
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  TextField(
+                    decoration: InputDecoration(
+                      labelText: 'Enter Cholesterol Level',
+                      hintText: 'For example 200 mg/dL',
+                      border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(11),
+                          borderSide: BorderSide(
+                            color: Color(0xFF055B5C),
+                          )),
+                      focusedBorder: OutlineInputBorder(
+                        // borderRadius: BorderRadius.circular(11.),
+                        borderSide: BorderSide(color: Color(0xFF055B5C)),
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                ],
+              ),
+            ),
+          ),
+
+          // Save button
+          Align(
+            alignment: Alignment.centerRight,
+            child: Container(
+              padding:
+                  EdgeInsets.only(right: 16.0), // Adjust the padding as needed
+              child: ElevatedButton(
+                onPressed: () {},
+                style: ElevatedButton.styleFrom(
+                  primary: Color(0xFF055B5C), // Set the desired color
+                ),
+                child: const Text('Save'),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
