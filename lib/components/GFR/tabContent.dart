6013fb68b791257faf0f803ca626f1d767dd8479
hajrahibrahimvirk <92207921+hajrahibrahimvirk@users.noreply.github.com>
2023-12-22 07:15:59 +0500

    Update tabContent.dart

--- lib/components/GFR/tabContent.dart
+++ lib/components/GFR/tabContent.dart
@@ -73,8 +73,11 @@ class _Tab1ContentState extends State<Tab1Content> {
             },
             child: Text('Calculate eGFR'),
           ),
-          SizedBox(height: 20),
-          Text('eGFR: $eGFR'),
+          SizedBox(height: 10),
+          Text(
+            'eGFR: $eGFR',
+            style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+          ),
         ],
       ),
     );
@@ -101,26 +104,148 @@ class Tab2Content extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: Text(
-          'GFR (mL/min/1.73 m2) = 175 × (Scr)-1.154 × (Age)-0.203 × (0.742 if female)'),
+      child: RichText(
+        textAlign: TextAlign.center,
+        text: TextSpan(
+          style: TextStyle(
+            fontSize: 18,
+            color: Colors.black, // Adjust the color as needed
+          ),
+          children: [
+            TextSpan(
+              text: 'GFR (mL/min/1.73 m',
+            ),
+            TextSpan(
+              text: '2',
+              style: TextStyle(
+                fontSize: 14,
+                // baseline: TextBaseline.alphabetic,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            TextSpan(
+              text: ') = 175 × (Scr)',
+            ),
+            TextSpan(
+              text: '-1.154',
+              style: TextStyle(
+                fontSize: 14,
+                // baseline: TextBaseline.alphabetic,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            TextSpan(
+              text: ' × (Age)',
+            ),
+            TextSpan(
+              text: '-0.203',
+              style: TextStyle(
+                fontSize: 14,
+                // baseline: TextBaseline.alphabetic,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            TextSpan(
+              text: ' × (0.742 if female)',
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
 
 class Tab3Content extends StatelessWidget {
+  final List<String> bulletPoints = [
+    "Recommended method for estimating GFR in adults from the National Kidney Foundation.",
+    "Designed for use with laboratory creatinine values that are standardized to IDMS.",
+    "Estimates GFR from serum creatinine, age, sex, and race.",
+    "More accurate than the MDRD Study equation, particularly in people with higher levels of GFR.",
+    "The CKD-EPI equation is based on the same four variables as the MDRD Study equation, but uses a 2-slope 'spline' to model the relationship between estimated GFR and serum creatinine, and a different relationship for age, sex, and race.",
+    "Some clinical laboratories are still reporting GFR estimates using the MDRD Study equation.",
+    "The National Kidney Foundation has recommended that clinical laboratories should begin using the CKD-EPI equation to report estimated GFR.",
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: Text('Content for Tab 3'),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          for (String point in bulletPoints)
+            Padding(
+              padding: EdgeInsets.symmetric(vertical: 8.0, horizontal: 5.0),
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  SizedBox(
+                    width: 8.0,
+                    child: Icon(
+                      Icons.brightness_1,
+                      size: 8.0,
+                    ),
+                  ),
+                  SizedBox(width: 8.0),
+                  Expanded(
+                    child: Text(
+                      point,
+                      style: TextStyle(fontSize: 16.0),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+        ],
+      ),
     );
   }
 }
 
 class Tab4Content extends StatelessWidget {
+  final List<String> content = [
+    "Levey AS, Stevens LA, Schmid CH, et al. A new equation to estimate glomerular filtration rate. Ann Intern Med. May 5 2009; 150(9): 604-612.",
+    "Levey AS, Stevens LA. Estimating GFR using the CKD Epidemiology Collaboration (CKD-EPI) creatinine equation: more accurate GFR estimates, lower CKD prevalence estimates, and better risk predictions. Am J Kidney Dis. Apr 2010; 55(4): 622-627.",
+    "Matsushita K, Selvin E, Bash LD, Astor BC, Coresh J. Risk implications of the new CKD Epidemiology Collaboration (CKD-EPI) equation compared with the MORD Study equation for estimated GFR: the Atherosclerosis Risk in Communities (ARIC) Study. Am J Kidney Dis. Apr 201O; 55(4): 648-659.",
+    "White SL, Polkinghorne KR, Atkins RC, Chadban SJ. Comparison of the prevalence and mortality risk of CKD in Australia using the CKD Epidemiology Collaboration (CKD-EPI) and Modification of Diet in Renal Disease (MORD) Study GFR estimating equations: the AusDiab (Australian Diabetes, Obesity and Lifestyle) Study. Am J Kidney Dis. Apr 2010; 55(4): 660-670.",
+    "Becker BN, Vassalotti JA. A software upgrade: CKD testing in 2010. Am J Kidney Dis. Jan 2009; 55(1): 8-10.",
+    "Frequently Asked Questions About GFR Estimates. New York: The National Kidney Foundation; 2011.",
+    "National Kidney Foundation. KDOQI clinical practice guidelines for chronic kidney disease: evaluation, classification, and stratification. Am J Kidney Dis. Feb 2002; 39 (Suppl 1): S1-266.",
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: Text('Content for Tab 4'),
+      child: Container(
+        padding: EdgeInsets.all(10.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            for (String item in content)
+              Padding(
+                padding: EdgeInsets.symmetric(vertical: 8.0),
+                child: Row(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    SizedBox(
+                      width: 8.0,
+                      child: Icon(
+                        Icons.arrow_forward,
+                        size: 16.0,
+                      ),
+                    ),
+                    SizedBox(width: 8.0),
+                    Expanded(
+                      child: Text(
+                        item,
+                        style: TextStyle(fontSize: 16.0),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+          ],
+        ),
+      ),
     );
   }
 }
