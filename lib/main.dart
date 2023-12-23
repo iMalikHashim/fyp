12e2e772c900b916125f620cc6cfa1f4eac4f9ee
hajrahibrahimvirk <92207921+hajrahibrahimvirk@users.noreply.github.com>
2023-12-23 20:25:47 +0500

    integrating firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,10 @@
 // ignore_for_file: unused_import, prefer_const_constructors
 
-import 'dart:js';
-
 import 'package:flutter/material.dart';
 import 'package:fl_chart/fl_chart.dart';
+import 'package:firebase_core/firebase_core.dart'; // Add this line
 import 'package:fyp/components/addReports.dart';
+import 'package:fyp/firebase_options.dart';
 import 'components/dashboard.dart';
 import 'components/reportsDetails.dart';
 import 'components/viewReports.dart';
@@ -13,18 +13,21 @@ import 'components/bloodpressure.dart';
 import 'components/LoginSignup/LandingScreen.dart';
 import 'components/dietPlan.dart';
 
-void main() => runApp(MaterialApp(
-      routes: {
-        // home: (),
-        '/': (context) => Dashboard(),
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
 
-        // '/': (context) => DietPlan(),
-        '/viewReports': (context) => ViewReportScreen(),
-        '/diabetes': (context) => DiabetesScreen(),
-        '/addReports': (context) => AddReportScreen(),
-        '/reportsDetails': (context) => ReportsDetails(),
-        '/bloodpressure': (context) => BloodPressure(),
-        // 'landingScreen': (context) => LandingScreen(),
-        // Add other routes as needed
-      },
-    ));
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
+  runApp(MaterialApp(
+    routes: {
+      '/': (context) => Dashboard(),
+      '/viewReports': (context) => ViewReportScreen(),
+      '/diabetes': (context) => DiabetesScreen(),
+      '/addReports': (context) => AddReportScreen(),
+      '/reportsDetails': (context) => ReportsDetails(),
+      '/bloodpressure': (context) => BloodPressure(),
+    },
+  ));
+}
