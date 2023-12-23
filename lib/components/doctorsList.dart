75e978d94bf38c07765e8be672411cb9d1635499
OyeeHashi <hashimtop10@gmail.com>
2023-12-23 13:15:55 +0500

    Doctors profile + Listing

--- lib/components/doctorsList.dart
+++ lib/components/doctorsList.dart
@@ -1,20 +1,10 @@
 import 'package:flutter/material.dart';
 
 class DoctorListPage extends StatelessWidget {
-  final List<Map<String, String>> doctors = [
-    {
-      'name': 'Dr. John Doe',
-      'expertise': 'Nephrologist',
-      'location': 'New York',
-      'imageUrl': 'url_to_image'
-    },
-    {
-      'name': 'Dr. Jane Smith',
-      'expertise': 'Urologist',
-      'location': 'Los Angeles',
-      'imageUrl': 'url_to_image'
-    },
-    //Will add dynamic data
+  final List<Map<String, dynamic>> doctors = [
+    {'name': 'Dr. John Doe', 'expertise': 'Nephrologist', 'location': 'New York', 'imageUrl': 'url_to_image'},
+    {'name': 'Dr. Jane Smith', 'expertise': 'Urologist', 'location': 'Los Angeles', 'imageUrl': 'url_to_image'},
+    //We'll use dynamic data 
   ];
 
   @override
@@ -31,14 +21,12 @@ class DoctorListPage extends StatelessWidget {
               backgroundImage: NetworkImage(doctors[index]['imageUrl']!),
             ),
             title: Text(doctors[index]['name']!),
-            subtitle: Text(
-                '${doctors[index]['expertise']} - ${doctors[index]['location']}'),
+            subtitle: Text('${doctors[index]['expertise']} - ${doctors[index]['location']}'),
             onTap: () {
               Navigator.push(
                 context,
                 MaterialPageRoute(
-                  builder: (context) =>
-                      DoctorProfilePage(doctor: doctors[index]),
+                  builder: (context) => DoctorProfilePage(doctor: doctors[index]),
                 ),
               );
             },
@@ -50,7 +38,7 @@ class DoctorListPage extends StatelessWidget {
 }
 
 class DoctorProfilePage extends StatelessWidget {
-  final Map<String, String> doctor;
+  final Map<String, dynamic> doctor;
 
   DoctorProfilePage({required this.doctor});
 
@@ -58,27 +46,119 @@ class DoctorProfilePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Doctor Profile'),
+        title: Text("Doctor's Profile"),
       ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          CircleAvatar(
-            backgroundImage: NetworkImage(doctor['imageUrl']!),
-            radius: 50,
-          ),
-          SizedBox(height: 20),
-          Text(
-            doctor['name']!,
-            style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-          ),
-          SizedBox(height: 10),
-          Text(
-            '${doctor['expertise']} - ${doctor['location']}',
-            style: TextStyle(fontSize: 16),
-          ),
-        ],
+      body: SingleChildScrollView(
+        padding: EdgeInsets.all(16),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            CircleAvatar(
+              backgroundImage: NetworkImage(doctor['imageUrl']!),
+              radius: 50,
+            ),
+            SizedBox(height: 20),
+            Text(
+              doctor['name']!,
+              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+            ),
+            SizedBox(height: 10),
+            Text(
+              '${doctor['expertise']} - ${doctor['location']}',
+              style: TextStyle(fontSize: 16),
+            ),
+            SizedBox(height: 20),
+            Text(
+              'Introduction:',
+              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+            ),
+            Text(
+              'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod odio vel risus tincidunt, non aliquam justo vulputate.',
+              style: TextStyle(fontSize: 16),
+            ),
+            SizedBox(height: 20),
+            Text(
+              'Available Dates:',
+              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+            ),
+            SizedBox(
+              height: 100,
+              child: ListView.builder(
+                scrollDirection: Axis.horizontal,
+                itemCount: 7,
+                itemBuilder: (context, index) {
+                  // You may want to replace this with actual dates
+                  return Container(
+                    margin: EdgeInsets.all(8),
+                    padding: EdgeInsets.all(8),
+                    decoration: BoxDecoration(
+                      border: Border.all(color: Colors.grey),
+                      borderRadius: BorderRadius.circular(8),
+                    ),
+                    child: Center(
+                      child: Text('Date ${index + 1}'),
+                    ),
+                  );
+                },
+              ),
+            ),
+            SizedBox(height: 20),
+            Text(
+              'Available Timings:',
+              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+            ),
+            SizedBox(
+              height: 50,
+              child: ListView(
+                scrollDirection: Axis.horizontal,
+                children: [
+                  buildTiming('9:00 AM'),
+                  buildTiming('10:00 AM'),
+                  buildTiming('11:00 AM'),
+                  buildTiming('1:00 PM'),
+                  buildTiming('3:00 PM'),
+                ],
+              ),
+            ),
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {
+                showDialog(
+                  context: context,
+                  builder: (BuildContext context) {
+                    return AlertDialog(
+                      title: Text('Appointment Booked'),
+                      content: Text('Your appointment has been booked with ${doctor['name']}!'),
+                      actions: [
+                        TextButton(
+                          onPressed: () {
+                            Navigator.of(context).pop();
+                          },
+                          child: Text('OK'),
+                        ),
+                      ],
+                    );
+                  },
+                );
+              },
+              child: Text('Book Appointment'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget buildTiming(String timing) {
+    return Container(
+      margin: EdgeInsets.all(8),
+      padding: EdgeInsets.all(8),
+      decoration: BoxDecoration(
+        border: Border.all(color: Colors.grey),
+        borderRadius: BorderRadius.circular(8),
+      ),
+      child: Center(
+        child: Text(timing),
       ),
     );
   }
