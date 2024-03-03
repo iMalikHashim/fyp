786d5c19c45dceb03e2bf15eaf9ccd20c6e1814e
iMalikHashim <hashimtop10@gmail.com>
2024-03-03 20:13:14 +0500

    PatientProfile

--- lib/model/diabetesModel.dart
+++ lib/model/diabetesModel.dart
@@ -4,8 +4,9 @@ class HealthData {
   HealthData({required this.bloodSugarLevel, required this.gfrRate});
 }
 
-List<HealthData> Hdata = [
+List<HealthData> hdata = [
   HealthData(bloodSugarLevel: 12.50, gfrRate: 14),
+  //We will get this data from firebase (So, bloodsugar/gfr e.t.c)
   HealthData(bloodSugarLevel: 14.50, gfrRate: 15),
   HealthData(bloodSugarLevel: 18.50, gfrRate: 16),
 ];
