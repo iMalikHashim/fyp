60b404713bd7a20f47eb877421d17582c0b0c24e
iMalikHashim <hashimtop10@gmail.com>
2024-03-03 20:03:23 +0500

    diabetes model

--- lib/model/diabetesModel.dart
+++ lib/model/diabetesModel.dart
@@ -3,3 +3,9 @@ class HealthData {
   double gfrRate;
   HealthData({required this.bloodSugarLevel, required this.gfrRate});
 }
+
+List<HealthData> Hdata = [
+  HealthData(bloodSugarLevel: 12.50, gfrRate: 14),
+  HealthData(bloodSugarLevel: 14.50, gfrRate: 15),
+  HealthData(bloodSugarLevel: 18.50, gfrRate: 16),
+];
